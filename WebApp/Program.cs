711c8e9c2eff86f589ef6bce7186c700017682fa
Jakub Dudziński <jdudzinski101@gmail.com>
2021-04-24 13:47:04 +0200

    Added database cod to Program.cs

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace WebApp
 {
@@ -14,7 +14,28 @@ namespace WebApp
     {
         public static void Main(string[] args)
         {
-            WebConnector wc = new WebConnector("https://api.covid19api.com/");
+            WebConnector wc;
+            DataBase context = new DataBase();
+            wc = new WebConnector("https://api.covid19api.com/");
+
+            DateTime today = DateTime.Today;
+            bool existance = false;
+
+            /*if (context.GDB.Any(record => record.DateDataBase == today)) existance = true;
+            if (!existance)
+            {
+                wc.SetGlobalSummary();
+                GlobalData gd1 = JsonParser.ExtractSingleData<GlobalData>(wc.Connect(), "Global");
+                context.GDB.Add(new GlobalDataBase { TotalConfirmed = gd1.TotalConfirmed, DateDataBase = today });
+                context.SaveChanges();
+
+            }
+
+            if (existance) Console.WriteLine("Todays data already in the data base.");
+            else if (!existance) Console.WriteLine("Todays data added.");
+
+            Console.WriteLine("\n***Global data checked!***\n");
+            */
             CreateHostBuilder(args).Build().Run();
         }
 
